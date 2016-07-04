fb952c85557997afb9657e34c268e4a51c8a3328
hexiaogang <hexiaogang@camera360.com>
2016-07-04 14:29:30 +0800

    [何小刚]test

--- app/src/main/java/com/rx/test/MainActivity.java
+++ app/src/main/java/com/rx/test/MainActivity.java
@@ -15,6 +15,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
@@ -32,43 +33,91 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mImageView = (ImageView) findViewById(R.id.image);
-        mProgressBar = (ProgressBar)findViewById(R.id.progress_bar);
-        setImage();
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        // setImage();
+        //createObservable();
+        justObservable();
+    }
+
+    private void justObservable() {
+        Observable.just("hello rx")
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.d("Denny", s);
+                    }
+                });
+        Observable.just(1).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+
+            }
+        });
+
+    }
+
+    private void createObservable() {
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("hello rx");
+                subscriber.onCompleted();
+            }
+        });
+        Observable observableLambda = Observable.create((subscriber) -> {
+            subscriber.onNext("hello rx lambda");
+            subscriber.onCompleted();
+        });
+
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d("Denny", s);
+            }
+        };
+        observable.subscribe(subscriber);
+        observableLambda.subscribe(subscriber);
     }
 
     private void setImage() {
         Observable.just("http://img2.zol.com.cn/product/139/520/ceiiyGdejX0E.jpg")
-                .map(new Func1<String, Bitmap>() {
-                    @Override
-                    public Bitmap call(String s) {
-                        try {
-                            return loadPic(s);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        return null;
+                .map(s -> {
+                    try {
+                        return loadPic(s);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
+                    return null;
                 })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<Bitmap>() {
-                    @Override
-                    public void call(Bitmap bitmap) {
-                        mProgressBar.setVisibility(View.GONE);
-                        mImageView.setImageBitmap(bitmap);
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        mProgressBar.setVisibility(View.GONE);
-                        Log.e("Denny", "error");
-                    }
-                }, new Action0() {
-                    @Override
-                    public void call() {
-                        Log.e("Denny", "complete");
-                    }
-                });
+                .subscribe((Bitmap bitmap) -> {
+                    mProgressBar.setVisibility(View.GONE);
+                    mImageView.setImageBitmap(bitmap);
+                }, throwable -> {
+                    mProgressBar.setVisibility(View.GONE);
+                    Log.e("Denny", "error");
+                }, () -> Log.e("Denny", "complete"));
     }
 
     private Bitmap loadPic(String url) throws IOException {
